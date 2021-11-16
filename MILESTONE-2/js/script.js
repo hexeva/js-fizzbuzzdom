f8f276e0c9e8d86a8c94e479c5d56426a55041ed
hexeva <patrick79.g@gmail.com>
2021-11-16 15:48:35 +0100

    creating element in js

---
+++ MILESTONE-2/js/script.js
@@ -0,0 +1,17 @@
+// Scrivi un programma che stampi i numeri da 1 a 100.
+
+// Dato un container nel DOM, appendi un elemento html con il numero o la stringa corretta.
+
+// CREO IL CICLO FOR
+
+for ( let i = 1; i <= 100; i++){
+    // console.log(i);
+
+    // mi creo il nuovo elemento che dovrà popolare l'html
+
+    let boxUnit =`
+    <div class="box">1</div>
+    `;
+
+
+}
