a59badcd62c613d12c1a38d198868588c3746354
hexeva <patrick79.g@gmail.com>
2021-11-16 16:01:54 +0100

    adding value to boxes

--- MILESTONE-2/js/script.js
+++ MILESTONE-2/js/script.js
@@ -2,16 +2,27 @@
 
 // Dato un container nel DOM, appendi un elemento html con il numero o la stringa corretta.
 
+
+// creo la variabile che mi selezionerà quale elemnto io devo popolare
+
+const boxContainer = document.querySelector('.box-container')
+
+
 // CREO IL CICLO FOR
 
+
 for ( let i = 1; i <= 100; i++){
-    // console.log(i);
 
     // mi creo il nuovo elemento che dovrà popolare l'html
 
     let boxUnit =`
-    <div class="box">1</div>
+    <div class="box">${i}</div>
     `;
+
+    // vado a popolare il box container
+
+    boxContainer.innerHTML += boxUnit;
+    console.log(boxContainer)
 
 
 }
